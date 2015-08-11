3a68bca4b491a9c5a026ec06add76a27faa79b7a
Daniel Bimschas <daniel@bimschas.com>
2015-08-11 13:34:34 +0200

    Fixed lint warnings in demo unit test

--- test/hello.spec.js
+++ test/hello.spec.js
@@ -1,11 +1,11 @@
-describe ("first test suite", function(){
-  it ("2 should equal 2", function(){
-    expect("2").toBe("2");
+describe('first test suite', function() {
+  it('2 should equal 2', function() {
+    expect('2').toBe('2');
   });
-  it ("1 should not equal 2", function(){
-    expect("1").not.toBe("2");
+  it('1 should not equal 2', function() {
+    expect('1').not.toBe('2');
   });
-  it ("2 should not equal 3", function(){
-    expect("2").not.toBe("3");
+  it('2 should not equal 3', function() {
+    expect('2').not.toBe('3');
   });
 });
