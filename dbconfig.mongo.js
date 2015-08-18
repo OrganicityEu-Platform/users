9faa860d7d4e8c06775c6d9554cf3b148ad27340
Armin <armin.beganovic@alexandra.dk>
2015-08-18 13:12:15 +0200

    Forgot to add JSCS format.

--- dbconfig.mongo.js
+++ dbconfig.mongo.js
@@ -1,5 +1,5 @@
 // $ mongo scenarios dbconfig.mongo.js
 
-db.scenarios.ensureIndex({title:"text"},{summary:"text"},{narrative:"text"});
-shellPrint("MONGO: Created text indexes on title, narrative, summary for full text search.");
+db.scenarios.ensureIndex({title: 'text'},{summary: 'text'},{narrative: 'text'});
+shellPrint('MONGO: Created text indexes on title, narrative, summary for full text search.');
 
