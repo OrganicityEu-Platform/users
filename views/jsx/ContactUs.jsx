d5bc74faf60a45d58a53a8e5a5f331ba4aaa46f5
Florian Büther <buether@itm.uni-luebeck.de>
2015-10-23 12:54:30 +0200

    Next incremental version of Contact page.

--- views/jsx/ContactUs.jsx
+++ views/jsx/ContactUs.jsx
@@ -6,7 +6,54 @@ var Link = Router.Link;
 export default React.createClass({
   render: function() {
     return (
-        <div>Awesome.</div>
+      <div>
+        <h1>Concat Us</h1>
+        <div className="container">
+          <div className="col-md-6">
+            FAQ?
+          </div>
+          <div className="col-md-6">
+            If you have any questions about managing scenarios, voicing your
+            opinion, or this platform in general, do not hesitate to contact us:
+            <div className="row well">
+
+            <form className="form-horizontal">
+              <div className="form-group">
+                <label className="control-label col-sm-4" htmlFor="subject">
+                  Subject
+                </label>
+
+                <div className="col-sm-8">
+                  <input type="text" className="form-control" name="subject"
+                    id="subject" />
+                </div>
+              </div>
+
+              <div className="form-group">
+                <label className="control-label col-sm-4" htmlFor="body">
+                  Message
+                </label>
+
+                <div className="col-sm-8">
+                  <textarea className="form-control" name="body" id="body" />
+                </div>
+              </div>
+
+              <div className="form-group">
+                <div className="col-sm-4">
+                </div>
+                <div className="col-sm-8">
+                  <button type="button" className="btn btn-default"
+                    onClick={this.clickedSubmit}>
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </form>
+</div>
+          </div>
+        </div>
+      </div>
     );
   }
 });
