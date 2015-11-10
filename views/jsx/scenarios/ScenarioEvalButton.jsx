1e4c919d5443f657d61f45a0322c7a87ac2b5631
Dennis Boldt <boldt@itm.uni-luebeck.de>
2015-11-10 19:15:02 +0100

    Fix ORG-337 - The button ‘EVALUATE’ is present on the scenario preview

--- views/jsx/scenarios/ScenarioEvalButton.jsx
+++ views/jsx/scenarios/ScenarioEvalButton.jsx
@@ -5,15 +5,25 @@ var Router = require('react-router');
 var ScenarioEvalButton = React.createClass({
   mixins: [Router.Navigation],
   handleClick: function() {
-    this.transitionTo('scenarioEvalView', { uuid : this.props.scenario.uuid }, { version : this.props.scenario.version });
+    this.transitionTo('scenarioEvalView', {
+      uuid : this.props.scenario.uuid
+    }, {
+      version : this.props.scenario.version
+    });
+
     if (typeof this.props.onChange == 'function') {
       this.props.onChange();
     }
   },
   render: function() {
-      return (
-        <button className="oc-button-submit" onClick={this.handleClick}>EVALUATE</button>
-      );
+
+    if (!this.props.scenario.uuid) {
+      return null;
+    }
+
+    return (
+      <button className="oc-button-submit" onClick={this.handleClick}>EVALUATE</button>
+    );
   }
 });
 
