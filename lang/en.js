dc17870869f9d0d2a7c2a3e6712402f202211539
Dennis Boldt <boldt@itm.uni-luebeck.de>
2015-11-17 21:56:52 +0100

    Missed lang-file

---
+++ lang/en.js
@@ -0,0 +1,13 @@
+module.exports = {
+  ScenarioEditView : {
+    titleInfo : 'No author would publish without a good title. Keep it short and sweet.',
+    summaryInfo : 'If you could summarize in a tweet, how would it be?',
+    narrativeInfo : 'Narrative is the core of your scenario. While it may be tempting to write an epic, you are limited to 2500 characters, about the length of a highschool essay. Keep it focused.',
+    sectorsInfo : 'Sectors are the area in which your scenario pertains to. If your idea revolves around shopping, your sector would be "Retail".',
+    actorsInfo : 'Actors facilitators. While it may be tempting to choose your favorite big screen personality, think of those who could push your ideas forward.',
+    toolsInfo : 'Tools are instruments you would need to implement your scenario.',
+    imageuploadInfo : 'Please upload an image. File type must be JPEG or PNG with a width of at least width 1140 px',
+    //imageCopyrightInfo : '...',
+    //creditInfo : '...'
+  }
+};
