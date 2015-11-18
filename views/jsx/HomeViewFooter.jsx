4e4087bc77993a8bdc3876682d72446b26ead084
Dennis Boldt <boldt@itm.uni-luebeck.de>
2015-11-18 23:05:46 +0100

    ORG-358 - real text "Co-creating cities of the future"

--- views/jsx/HomeViewFooter.jsx
+++ views/jsx/HomeViewFooter.jsx
@@ -13,11 +13,9 @@ var HomeViewFooter = React.createClass({
           <div className="oc-home-view-footer">
             <h1 className="oc-home-view-title">Co-creating cities of the future</h1>
             <div className="col-md-6">
-              <p>Vestibulum id ligula porta felis euismod semper. Donec ullamcorper nulla non metus auctor fringilla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec sed odio dui. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <p>As the OrganiCity smart platform is being built, to meet the vision set through citizen discussions, constant input and feedback is required and encouraged. What data do you need? What devices do you wish to connect? How should OrganiCity call for experiments? How can we make that experimentation easier? Together, the city, businesses, researchers and citizens can create a better, more inclusive, more accessible smart city.</p>
             </div>
-            <div className="col-md-6">
-              <p>Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Maecenas faucibus mollis interdum. Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam id dolor id nibh ultricies vehicula ut id elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-            </div>
+            {/*
             <div className="col-md-6">
               <Accordion className="oc-top-accordian">
                 <Panel header={<i className="fa fa-plus oc-faq">Nulla vitae elit libero, a pharetra augue?</i>} eventKey="0">
@@ -62,6 +60,7 @@ var HomeViewFooter = React.createClass({
                 </Panel>
               </Accordion>
             </div>
+            */}
             <div className="col-md-4 col-md-offset-4">
               <a href="http://organicity.eu" target="_blank">
                 <Button className="oc-button">READ MORE ABOUT ORGANICITY</Button>
