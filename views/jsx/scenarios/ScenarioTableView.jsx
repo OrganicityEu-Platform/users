ba3eb8ebc28a8a6cd6864ad0aff75dcd8207f9a3
Armin Beganovic <armin.beganovic@gmail.com>
2015-11-19 01:34:09 +0100

    AST meta plural/singular form

--- views/jsx/scenarios/ScenarioTableView.jsx
+++ views/jsx/scenarios/ScenarioTableView.jsx
@@ -102,20 +102,20 @@ var ScenarioTableView = React.createClass({
           <div className="scenario-article-meta">
             <div className="col-md-3">
               <div className="scenario-ast-wrapper">
-                <span className="scenario-ast">Actor:</span>
+                <span className="scenario-ast">Actor(s):</span>
                 <span className="scenario-ast-items">{this.props.scenario.actors ? this.props.scenario.actors.join(', ') : ''}</span><br></br>
               </div>
 
             </div>
             <div className="col-md-3">
               <div className="scenario-ast-wrapper">
-                <span className="scenario-ast">Sector:</span>
+                <span className="scenario-ast">Sector(s):</span>
                 <span className="scenario-ast-items">{this.props.scenario.sectors ? this.props.scenario.sectors.join(', ') : ''}</span><br></br>
               </div>
             </div>
             <div className="col-md-3">
               <div className="scenario-ast-wrapper">
-                <span className="scenario-ast">Tool:</span>
+                <span className="scenario-ast">Tool(s):</span>
                 <span className="scenario-ast-items">{this.props.scenario.devices ? this.props.scenario.devices.join(', ') : ''}</span><br></br>
               </div>
             </div>
