f58b5587e4595a12614f5f8870e7f07c0bd91b7b
Armin Beganovic <armin.beganovic@gmail.com>
2016-01-02 18:24:12 +0100

    human test #5 + navbar cta alignment

--- views/jsx/HomeViewHeader.jsx
+++ views/jsx/HomeViewHeader.jsx
@@ -9,7 +9,7 @@ var HomeViewHeader = React.createClass({
         <div className="oc-home-view-header-wrapper">
           <div className="oc-home-view-header container">
             <h1 className="oc-home-view-header-text">Welcome to OrganiCity Scenarios</h1>
-            <h2 className="col-md-8 col-md-offset-2 oc-home-view-header-sub-text">Making cities better</h2>
+            <h2 className="col-md-8 col-md-offset-2 oc-home-view-header-sub-text">Co-creating smart cities of the future</h2>
           </div>
         </div>
       </div>
