7fcbe1de6080a577db71935d49bb5b79d30b27e5
Dennis Boldt <boldt@itm.uni-luebeck.de>
2016-03-01 15:17:16 +0100

    ORG-201 - added google analytics

--- views/jsx/App.jsx
+++ views/jsx/App.jsx
@@ -2,11 +2,32 @@ import $          from 'jquery';
 import React      from 'react';
 import routes     from './Routes.jsx';
 
+import ga_config  from '../../config/ga.js';
+
+console.log(ga_config);
+
 var Router = require('react-router');
 var Route = Router.Route;
 
+var ga = require('react-ga');
+
 $(function() {
-  Router.run(routes, Router.HistoryLocation, function(Root) {
-    React.render(<Root />, document.body);
+  // Google Analytics
+  var options = { debug: true };
+  ga.initialize('UA-74522426-1', options);
+
+  Router.run(routes, Router.HistoryLocation, function(Handler, state) {
+
+    // track page views
+    if(ga_config.debug) {
+      console.log('State:', state);
+    }
+    var options = {
+       debug: ga_config.debug
+    };
+    ga.initialize(ga_config.id, options);
+
+    // Render page
+    React.render(<Handler/>, document.body);
   });
 });
