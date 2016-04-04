99094a94584cc7b69d8e50563206943c2f5fe754
Dennis Boldt <boldt@itm.uni-luebeck.de>
2016-04-04 19:29:58 +0200

    ORG-469 - further steps to oauth2 by fixing C&P errors

--- config/passport.js
+++ config/passport.js
@@ -552,11 +552,11 @@ module.exports = function(passport) {
               // if there is a user id already but no token (user was linked at one point and then removed)
               if (!user.disqus.token) {
                 user.disqus.token = token;
-                newUser.disqus.name = profile.displayName;
+                user.disqus.name = profile.displayName;
 
                 var raw = JSON.parse(profile._raw);
                 var email = raw.response.email;
-                newUser.disqus.email = (email || '').toLowerCase(); // pull the first email
+                user.disqus.email = (email || '').toLowerCase(); // pull the first email
 
                 user.save(function(err) {
                   if (err) {
@@ -597,7 +597,9 @@ module.exports = function(passport) {
           user.disqus.id = profile.id;
           user.disqus.token = token;
           user.disqus.name = profile.displayName;
-          user.disqus.email = profile.eMail;
+          var raw = JSON.parse(profile._raw);
+          var email = raw.response.email; // CHECK
+          newUser.oauth.email = (email || '').toLowerCase(); // pull the first email
 
           user.save(function(err) {
             if (err) {
@@ -626,9 +628,15 @@ module.exports = function(passport) {
 
       // asynchronous
       process.nextTick(function() {
+
+        console.log('oAuth2 strategy req', req);
+        console.log('oAuth2 strategy token', token);
+        console.log('oAuth2 strategy refreshToken', refreshToken);
+        console.log('oAuth2 strategy profile', profile);
+        console.log('oAuth2 strategy profile.id', profile.id);
+
         // check if the user is already logged in
         if (!req.user) {
-          console.log('oAuth2 strategy', profile);
           User.findOne({
             'oauth.id': profile.id
           }, function(err, user) {
@@ -637,36 +645,29 @@ module.exports = function(passport) {
             }
             if (user) {
 
-              /*
               // if there is a user id already but no token (user was linked at one point and then removed)
-              if (!user.disqus.token) {
-                user.disqus.token = token;
-                newUser.disqus.name = profile.displayName;
+              if (!user.oauth2.token) {
 
-                var raw = JSON.parse(profile._raw);
-                var email = raw.response.email;
-                newUser.disqus.email = (email || '').toLowerCase(); // pull the first email
+                user.oauth2.token = token;
+                user.oauth2.name = profile.displayName;
+                user.oauth2.email = profile.email;
 
                 user.save(function(err) {
                   if (err) {
                     return done(err);
                   }
-
                   return done(null, user);
                 });
+
               }
-              */
               return done(null, user);
             } else {
               var newUser = new User();
               newUser.uuid = uuid.v4();
-              newUser.oauth.id = profile.id;
-              newUser.oauth.token = token;
 
-              newUser.oauth.name = profile.displayName;
-              var raw = JSON.parse(profile._raw);
-              var email = raw.response.email; // CHECK
-              newUser.oauth.email = (email || '').toLowerCase(); // pull the first email
+              newUser.oauth2.token = token;
+              newUser.oauth2.name = profile.displayName;
+              newUser.oauth2.email = profile.email;
 
               newUser.save(function(err) {
                 if (err) {
@@ -682,12 +683,10 @@ module.exports = function(passport) {
           // user already exists and is logged in, we have to link accounts
           var user = req.user; // pull the user out of the session
 
-          user.disqus.id = profile.id;
-          user.disqus.token = token;
-          user.disqus.name = profile.displayName;
-          var raw = JSON.parse(profile._raw);
-          var email = raw.response.email;
-          user.disqus.email = (email || '').toLowerCase(); // pull the first email
+          newUser.oauth2.id = profile.id;
+          newUser.oauth2.token = token;
+          newUser.oauth2.name = profile.displayName;
+          newUser.oauth2.email = profile.email;
 
           user.save(function(err) {
             if (err) {
