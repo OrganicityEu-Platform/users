b7be5e4bf58d9a304ca500e6c1610b58a265b3f4
Armin Beganovic <armin.beganovic@gmail.com>
2016-05-29 01:04:46 +0200

    quick-fix to eco

--- views/jsx/scenarios/ScenarioEco.jsx
+++ views/jsx/scenarios/ScenarioEco.jsx
@@ -9,26 +9,37 @@ var ScenarioEco = React.createClass({
   mixins: [LoadingMixin],
   getInitialState: function () {
     return {
-      items: []
+      items: [],
+      noMatch: [],
+      match: ['transport', 'environment', 'cultural', 'retail', 'energy', 'public', 'healthcare', 'agriculture'],
     };
   },
   componentDidMount: function() {
 
     var options = {
       type : 'scenarios_by_sector',
-      match: ['transport', 'environment', 'cultural', 'retail', 'energy', 'public', 'healthcare']
+      match: this.state.match
     };
 
     var url = api.reverse('statistics', options);
-    console.log(url);
+
     $.ajax(url, {
       dataType : 'json',
-      success : (result) => {
-        this.setState({
-          items: result
-        });
-      }
+      success : this.handleData
     });
+  },
+  handleData: function(data) {
+
+    var dataItems = [];
+
+    var i;
+    for(i = 0; i < data.length; i++) { dataItems.push(data[i]._id); }
+
+    this.setState({
+      noMatch: $(this.state.match).not(dataItems).get(),
+      items: data
+    });
+
   },
   render: function() {
     return(
@@ -45,6 +56,22 @@ var ScenarioEco = React.createClass({
               </span>
               <span>
                 {item.count} scenarios
+              </span>
+            </div>
+          )
+        }
+        {
+          this.state.noMatch.map(
+            (item, i) =>
+            <div key={i} className={"oc-eco-item"}>
+              <img
+                className="oc-eco-item-icon"
+                src={ui.asset('static/img/'.concat(item.concat('_icon.svg')))}/>
+              <span className={"oc-eco-item-text" + " " + item + "_colour_txt"}>
+                {item.toUpperCase()}
+              </span>
+              <span>
+                0 scenarios
               </span>
             </div>
           )
