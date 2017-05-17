8161a70fbf1df2c7e72700e39143a60a91cc287e
Dennis Boldt <boldt@itm.uni-luebeck.de>
2017-05-17 15:24:43 +0200

    Cache internal token

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -37,45 +37,59 @@ var cron = require('cron');
 
 var https = require('https');
 
+// 3,5 Minutes
+var timeAccessToken = ((3 * 60) + 30);
+
 var getToken = function(callback) {
 
   console.time('Get token');
-  console.log(callback);
 
-  var payload = 'grant_type=client_credentials' +
-    '&client_id=' + config.client_id +
-    '&client_secret=' + config.client_secret;
+  redis.get('oc.users.accessToken', function(err, reply) {
+    if (err || !reply) {
+      console.log('No access token in cache. Renew token!');
 
-  var options = {
-    host: 'accounts.organicity.eu',
-    path: '/realms/organicity/protocol/openid-connect/token',
-    port: 443,
-    method: 'POST',
-    headers: {
-      'Content-Type' : 'application/x-www-form-urlencoded',
-      'Content-Length': Buffer.byteLength(payload)
+      var payload = 'grant_type=client_credentials' +
+          '&client_id=' + config.client_id +
+          '&client_secret=' + config.client_secret;
+
+      var options = {
+        host: 'accounts.organicity.eu',
+        path: '/realms/organicity/protocol/openid-connect/token',
+        port: 443,
+        method: 'POST',
+        headers: {
+          'Content-Type' : 'application/x-www-form-urlencoded',
+          'Content-Length': Buffer.byteLength(payload)
+        }
+      };
+
+      // Call roles endpoint to get the subs for the corresponding role name
+      var req = https.request(options, function(res) {
+        var str = '';
+        res.on('data', function(chunk) {
+          str += chunk;
+        });
+
+        res.on('end', function() {
+          var json = JSON.parse(str);
+          console.timeEnd('Get token');
+          redis.setex('oc.users.accessToken', timeAccessToken, json.access_token, function() {
+            callback(json.access_token);
+          });
+
+        });
+      });
+      req.write(payload);
+      req.end();
+
+      req.on('error', function(e) {
+        console.error(e);
+      });
+
+    } else {
+      console.log('Access token in cache. Reuse!');
+      callback(reply.toString());
     }
-  };
-
-  // Call roles endpoint to get the subs for the corresponding role name
-  var req = https.request(options, function(res) {
-    var str = '';
-    res.on('data', function(chunk) {
-      str += chunk;
-    });
-
-    res.on('end', function() {
-      //console.log(res.statusCode);
-      var json = JSON.parse(str);
-      console.timeEnd('Get token');
-      callback(json.access_token);
-    });
-  });
-  req.write(payload);
-  req.end();
-
-  req.on('error', function(e) {
-    console.error(e);
   });
 };
 
@@ -497,7 +511,6 @@ module.exports = function(router, passport) {
         });
 
         res2.on('end', function() {
-          console.log(str);
           if (res.statusCode === 200) {
             var json = JSON.parse(str);
             var interests = [];
@@ -511,13 +524,6 @@ module.exports = function(router, passport) {
           } else {
             res.send(500, 'Internal Server Error');
           }
-          /*
-          //console.log(role);
-          console.timeEnd(redis_prefix + role);
-          console.log(res.statusCode + ' for get users with role ' + role);
-            redis.set(redis_prefix + role , str);
-          }
-          */
         });
       });
       req.end();
